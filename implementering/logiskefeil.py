f997db6c99229633e0811e9a0cab7a8e09f3fabb
TrulsLien <slurt360@gmail.com>
2023-03-30 13:56:20 +0200

    Oppdatering

--- implementering/logiskefeil.py
+++ implementering/logiskefeil.py
@@ -1,9 +1,9 @@
-assert 10 > 5 # 10 er større enn 5, derfor gjør denne ingenting
+# assert 10 > 5 # 10 er større enn 5, derfor gjør denne ingenting
 
-try:
-    assert 10 > 20 # 10 er ikke større enn 20, derfor gir denne en feilmeldig
-except:
-    print("Hei på deg!")
+# try:
+#     assert 10 > 20 # 10 er ikke større enn 20, derfor gir denne en feilmeldig
+# except:
+#     print("Hei på deg!")
 
 while True:
     try:
@@ -18,18 +18,18 @@ def areal(hoyde, bredde):
 def omkrets(hoyde, bredde):
     return hoyde + hoyde + bredde + bredde
 
-print(f"Arealet av rektangelet er: {areal}")
+print(f"Arealet av rektangelet er: {areal(hoyde, bredde)}")
 
 
-assert areal(3,2) == 6
-assert areal(3,3) == 9
-assert areal(3,4) == 12
-assert omkrets(3,2) == 10
-assert omkrets(3,3) == 12
-assert omkrets(3,4) == 14
+# assert areal(3,2) == 6
+# assert areal(3,3) == 9
+# assert areal(3,4) == 12
+# assert omkrets(3,2) == 10
+# assert omkrets(3,3) == 12
+# assert omkrets(3,4) == 14
 
 
 
 
-print("Koden er ferdig")
+# print("Koden er ferdig")
 
